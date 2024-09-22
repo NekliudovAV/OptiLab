003b34218fe53016535c7d5b31c8a635a76ebef6
NekliudovAV <68639050+NekliudovAV@users.noreply.github.com>
2024-09-22 21:56:27 +0300

    Update utils.py

--- OldFiles/utils.py
+++ OldFiles/utils.py
@@ -1,3 +1,4 @@
+from influxdb import DataFrameClient
 def write_pyomo_results_2_influxDB(resdf, calc_type='Optimize', database_='TES',  time_zone_ = None, tags_=None, host='127.0.0.1'):
     # tags_={'Scenarii':'MinD0'}
     influxDataFrameClient_client = DataFrameClient(host=host, port=8086, database=database_)
