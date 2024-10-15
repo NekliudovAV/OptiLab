043aef6cc0d8b87b1533f1c3d89451b445634690
NekliudovAV <68639050+NekliudovAV@users.noreply.github.com>
2024-10-15 10:16:49 +0300

    Update database.py

--- optilab/database.py
+++ optilab/database.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 from pymongo import MongoClient
 from influxdb import DataFrameClient
 
@@ -62,12 +63,14 @@ def read_DF_from_influxDB(host_ = None,
                           database_ = None,
                           table_ = None,
                           timestamp_ = None,
+                          timestamp_to = None,
                           time_zone_ = None):
     """
     Запрос из БД InfluxDB предрасчетный параметров 
     Возвращает dataframe с предрасчетными параметрами
     """
-    #t0 = time.time()
+    t0=time.time()
+    timestamp_=pd.Timestamp(timestamp_)
     if host_==None:
         host_=config.INFLUX['IP_']
     if port_==None:    
@@ -78,11 +81,19 @@ def read_DF_from_influxDB(host_ = None,
         time_zone_ = 'Etc/GMT-3'
     #print('port_',port_, type(port_))    
     influxDataFrameClient_client = DataFrameClient(host = host_, port = port_, database = database_)
-    influx_DBname = table_
-    df = influxDataFrameClient_client.query(f"""select * from {influx_DBname} where time = '{timestamp_}' tz('{time_zone_}')""")[influx_DBname]    
+    
+    if timestamp_to == None:
+        timestamp_to=pd.Timestamp(timestamp_)
+    else:
+        timestamp_to=pd.Timestamp(timestamp_to)
+    
+    query=f"""select * from {table_} where time >= '{timestamp_}'  and time <= '{timestamp_to}' tz('{time_zone_}')"""
+    #print(query)
+    df = influxDataFrameClient_client.query(query)[table_]    
     df = df.tz_convert(time_zone_)
+    
     influxDataFrameClient_client.close()
-    # dt = time.time() - t0
-    #print(f'Запрос данных по {df.shape[1]} каналам измерений за {df.shape[0]} периодов выполнен за {dt: 3.3f} c')
+    dt = time.time() - t0
+    print(f'Запрос на получение данных из {table_} c {timestamp_} по {timestamp_to}  выполнен за {dt: 3.3f} c')
     return df
 
