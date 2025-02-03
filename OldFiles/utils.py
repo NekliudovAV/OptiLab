63dafbe04ad7cc0b2b8b9ad681216005bd249385
NekliudovAV <68639050+NekliudovAV@users.noreply.github.com>
2025-02-03 17:27:32 +0300

    Delete OldFiles/utils.py

--- OldFiles/utils.py
+++
@@ -1,7 +0,0 @@
-from influxdb import DataFrameClient
-def write_pyomo_results_2_influxDB(resdf, calc_type='Optimize', database_='TES',  time_zone_ = None, tags_=None, host='127.0.0.1'):
-    # tags_={'Scenarii':'MinD0'}
-    influxDataFrameClient_client = DataFrameClient(host=host, port=8086, database=database_)
-    influx_DBname = calc_type
-    influxDataFrameClient_client.write_points(resdf.astype(float), influx_DBname, tags=tags_, batch_size=1000)
-    influxDataFrameClient_client.close()
