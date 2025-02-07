fd8e8dd5f33ac97312cd7eed5837ec7e259fc883
NekliudovAV <68639050+NekliudovAV@users.noreply.github.com>
2025-02-07 14:08:40 +0300

    Add files via upload

---
+++ Examples/config.py
@@ -0,0 +1,3 @@
+VERSION = '1'
+MONGO={'DB_name':'KEM_GRES', 'IP_':'10.16.0.157', 'port_' : 27017, 'username_':'mongo', 'password_':'mongo'}
+INFLUX={'DB_name':'KEM_GRES', 'IP_':'10.16.0.157', 'port_' : 8086}
