947e4ae02d60dfe6ae06081e75b629864caf04a5
NekliudovAV <68639050+NekliudovAV@users.noreply.github.com>
2025-03-12 17:11:34 +0300

    Update prepare_curvs.py

--- optilab/prepare_curvs.py
+++ optilab/prepare_curvs.py
@@ -13,6 +13,13 @@ import plotly.express as px
 from scipy.spatial  import ConvexHull
 from pyomo.environ import *
 from df2block import *
+from database import *
+
+import plotly.graph_objects as go # импортируем библиотеку graph_objects для 3D отображения
+from plotly.subplots import make_subplots # подключаем возможность разбиения блока на два блока
+from sklearn.linear_model import LinearRegression
+
+
 
 
 def calc_H(P,T):
@@ -175,4 +182,107 @@ def plot_corrected(out,MData,cols):
 
         fig.update_layout(height=600, width=800, title_text="Измеренные и скорректированные")
         fig.show()
+
+
+
+
+
+def sort_columns_by_uvalues(Qt):
+    df={}
+    for k in Qt.keys():
+        df[k]=[len(Qt[k].unique())]
+    df=pd.DataFrame(df).transpose()
+    return Qt[list(df.sort_values(by=[0]).index)] 
     
+def plot_df(title_,dfI):
+    dfI=dfI.copy()
+    dfI_table=dfI.copy()
+    # Корректируем размерность df для отображения:
+    # Если параметр в колонке не менятеся, удаляем колонку
+    for k in dfI.keys():
+        if len(dfI[k].unique())==1:
+            dfI=dfI.drop(columns=[k])
+    #
+    k=1.8
+    VarNames=list(dfI.keys()) # формирование списка
+    camera = dict(eye=dict(x=k, y=0.3, z=k)) # формирование словаря
+    # разбиваем графическую область на одну строку и дву колонки
+    if len(VarNames)<3:    
+        type_plot= 'scatter'
+    else:
+        type_plot= 'surface'
+        
+    fig = make_subplots(
+        rows=1, cols=2,specs=[[{'type': type_plot},{'type':'table'}]])
+
+    # Отрисовка характеристики
+    # Выбор уникального значения по определенному параметру
+    if dfI.shape[1]>3:
+        for tDm in dfI[VarNames[-4]].unique():
+            index=dfI[VarNames[-4]]==tDm # выбор индекса, соответствующего уникальному значению и выполнение среза
+            # создаем 3D объект на основе характеристик данных, полученных функцией и размещаем, созданный объект в первом столбце
+            fig.add_trace(go.Mesh3d(x=dfI[VarNames[-3]][index],y=dfI[VarNames[-2]][index],z=dfI[VarNames[-1]][index],
+                                    colorscale="BuGn", opacity=0.9,hovertext=VarNames[0]+'='+str(tDm)),row=1, col=1)
+    elif dfI.shape[1]==3:
+            fig.add_trace(go.Mesh3d(x=dfI[VarNames[-3]],y=dfI[VarNames[-2]],z=dfI[VarNames[-1]],colorscale='amp',
+                                     opacity=0.9,hovertext=VarNames[0]+'='+str('')),row=1, col=1)
+    elif dfI.shape[1]<3:
+            fig.add_trace(go.Scatter(x=dfI[VarNames[-2]].values,y=dfI[VarNames[-1]].values,
+                                      opacity=0.9,hovertext=VarNames[-1]),row=1,col=1)
+        
+            
+    if dfI.shape[1]<3:
+        fig.update_layout(scene = dict(
+                        xaxis_title=VarNames[-2],
+                        yaxis_title=VarNames[-1]),showlegend = True,
+                     scene_camera=camera,width=1000,height=600,title=title_)    
+    else:    
+        fig.update_layout(scene = dict(
+                        xaxis_title=VarNames[-3],
+                        yaxis_title=VarNames[-2],
+                        zaxis_title=VarNames[-1]),showlegend = True,
+                     scene_camera=camera,width=1000,height=600,title=title_)    
+        
+    # Отрисовка таблицы
+    fig.add_trace(go.Table(
+        header=dict(values=list(dfI_table.columns), align='left'),
+        cells=dict(values=dfI_table.values.transpose(), align='left')),
+                  row=1, col=2)
+    #fig.show()
+    return fig    
+    
+
+def slice_(Qt,fix_values=None):
+    if fix_values==None:
+        Fixed_fils=['H0','T0','P0', 'Dd','P2','IWF','dGfwD0','Dsp','IDsp','Pmix']
+        fix_values=set(Fixed_fils).intersection(Qt.keys())
+
+    F=[Qt.keys()[-1]]
+    lm = LinearRegression()
+    lm.fit(Qt.iloc[:,:-1],Qt.iloc[:,-1])
+    # Расчёт погрешности:
+    Fit=lm.predict(Qt.iloc[:,:-1])
+    print('Погрешность slice-ера, %',((Qt.iloc[:,-1]-Fit)/Qt.iloc[:,-1]).abs().mean())
+    
+    Data=Qt.copy()
+    for k in fix_values:
+        Data[k]=Data[k][Data[k]!=0].mean()
+    t=(set(Data.keys())-set(fix_values))-set(F) 
+    t=list(t)
+    t.extend(F)
+
+    Keys=list(Data.keys())
+    Fit=lm.predict(Data[Keys[:-1]])
+    Data[Keys[-1]]=Fit
+    Data=Data.sort_values(by=F)
+    return Data
+    
+def plot_df_dim(df,fix_values=None,Name='',sort_Columns=True):
+    # вЫПОЛНЯЕТ ОТРИСОВКУ ХАРАКЕТРИСТИК
+    print('shape df:',df.shape[1])
+    if df.shape[1]>4:
+        df=slice_(df,fix_values=None)
+    if sort_Columns:    
+        df=sort_columns_by_uvalues(df)
+    df=df.round(1)    
+    return plot_df(f'Характeристика:{Name}',df)    
